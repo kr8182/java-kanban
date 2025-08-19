c56a321e917b932dd1e43d6a76762223e7f520fb
kr8182 <kr81828@gmail.com>
2025-08-19 19:11:07 +0300

    Переделал тесты

--- test/EpicTest.java
+++ test/EpicTest.java
@@ -10,11 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EpicTest {
 
-    private FileBackedTaskManager manager;
-    private int epicId;
+    private static FileBackedTaskManager manager;
+    private static int epicId;
 
-    @BeforeEach
-    public void beforeEach() {
+    @BeforeAll
+    public static void beforeAll() {
         manager = Managers.getDefaultFileManager();
         Epic epic = new Epic(0, "Эпик 1", "Эпик 1_Тест");
         manager.createEpic(epic);
@@ -30,34 +30,34 @@ class EpicTest {
         } catch (ManagerSaveException e) {
             throw new RuntimeException(e);
         }
-        assertEquals("[2]", epicFromManager.getSubtaskIds().toString());
+        assertEquals("[3, 4, 5, 6]", epicFromManager.getSubtaskIds().toString());
     }
 
     @Test
     void testAddSubtaskId() {
         Epic epicFromManager = manager.getEpic(epicId);
-        SubTask subtask1 = new SubTask(2, "СабТаска1", "СабТаска1_Тест", epicId);
-        assertEquals("[]", epicFromManager.getSubtaskIds().toString());
+        SubTask subtask2 = new SubTask(2, "СабТаска1", "СабТаска1_Тест", epicId);
+        assertEquals("[3, 4, 5, 6]", epicFromManager.getSubtaskIds().toString());
         try {
-            manager.createSubTask(subtask1);
+            manager.createSubTask(subtask2);
         } catch (ManagerSaveException e) {
             throw new RuntimeException(e);
         }
-        assertEquals("[2]", epicFromManager.getSubtaskIds().toString());
+        assertEquals("[3, 4, 5, 6, 7]", epicFromManager.getSubtaskIds().toString());
     }
 
     @Test
     void testRemoveSubtaskId() {
         Epic epicFromManager = manager.getEpic(epicId);
-        SubTask subtask1 = new SubTask(3, "СабТаска1", "СабТаска1_Тест", epicId);
+        SubTask subtask3 = new SubTask(3, "СабТаска1", "СабТаска1_Тест", epicId);
         assertEquals("[]", epicFromManager.getSubtaskIds().toString());
         try {
-            manager.createSubTask(subtask1);
+            manager.createSubTask(subtask3);
         } catch (ManagerSaveException e) {
             throw new RuntimeException(e);
         }
         assertEquals("[2]", epicFromManager.getSubtaskIds().toString());
-        manager.deleteSubTask(subtask1.getTaskId());
+        manager.deleteSubTask(subtask3.getTaskId());
         assertEquals("[]", epicFromManager.getSubtaskIds().toString());
     }
 
@@ -65,18 +65,18 @@ class EpicTest {
     void testEpicStatus() {
         Epic epicFromManager = manager.getEpic(epicId);
 
-        SubTask subtask1 = new SubTask(4, "СабТаска1", "СабТаска1_Тест", epicId);
-        SubTask subtask2 = new SubTask(5, "СабТаска2", "СабТаска2_Тест", epicId);
-        SubTask subtask3 = new SubTask(6, "СабТаска3", "СабТаска3_Тест", epicId);
+        SubTask subtask4 = new SubTask(4, "СабТаска1", "СабТаска1_Тест", epicId);
+        SubTask subtask5 = new SubTask(5, "СабТаска2", "СабТаска2_Тест", epicId);
+        SubTask subtask6 = new SubTask(6, "СабТаска3", "СабТаска3_Тест", epicId);
 
-        manager.createSubTask(subtask1);
-        manager.createSubTask(subtask2);
+        manager.createSubTask(subtask4);
+        manager.createSubTask(subtask5);
 
-        subtask1.setDuration(Duration.ofHours(25));
-        subtask2.setDuration(Duration.ofHours(49));
-        subtask1.setStatus(TaskStatus.IN_PROGRESS);
+        subtask4.setDuration(Duration.ofHours(25));
+        subtask5.setDuration(Duration.ofHours(49));
+        subtask6.setStatus(TaskStatus.IN_PROGRESS);
 
-        manager.createSubTask(subtask3);
+        manager.createSubTask(subtask6);
 
         assertEquals("IN_PROGRESS", epicFromManager.getStatus().toString());
     }
@@ -85,18 +85,18 @@ class EpicTest {
     void testEpicStatusDone() {
         Epic epicFromManager = manager.getEpic(epicId);
 
-        SubTask subtask1 = new SubTask(7, "СабТаска1", "СабТаска1_Тест", epicId);
-        SubTask subtask2 = new SubTask(8, "СабТаска2", "СабТаска2_Тест", epicId);
-        SubTask subtask3 = new SubTask(9, "СабТаска3", "СабТаска3_Тест", epicId);
+        SubTask subtask7 = new SubTask(7, "СабТаска1", "СабТаска1_Тест", epicId);
+        SubTask subtask8 = new SubTask(8, "СабТаска2", "СабТаска2_Тест", epicId);
+        SubTask subtask9 = new SubTask(9, "СабТаска3", "СабТаска3_Тест", epicId);
 
-        manager.createSubTask(subtask1);
-        manager.createSubTask(subtask2);
+        manager.createSubTask(subtask7);
+        manager.createSubTask(subtask8);
 
-        subtask1.setDuration(Duration.ofHours(25));
-        subtask2.setDuration(Duration.ofHours(49));
-        subtask1.setStatus(TaskStatus.DONE);
+        subtask7.setDuration(Duration.ofHours(25));
+        subtask8.setDuration(Duration.ofHours(49));
+        subtask9.setStatus(TaskStatus.DONE);
 
-        manager.createSubTask(subtask3);
+        manager.createSubTask(subtask9);
 
         assertEquals("IN_PROGRESS", epicFromManager.getStatus().toString());
     }
@@ -105,13 +105,15 @@ class EpicTest {
     void testEpicStatusDoneAll() {
         Epic epicFromManager = manager.getEpic(epicId);
 
-        SubTask subtask1 = new SubTask(10, "СабТаска1", "СабТаска1_Тест", TaskStatus.DONE, epicId);
-        SubTask subtask2 = new SubTask(11, "СабТаска2", "СабТаска2_Тест", TaskStatus.DONE, epicId);
-        SubTask subtask3 = new SubTask(12, "СабТаска3", "СабТаска3_Тест", TaskStatus.DONE, epicId);
+        manager.getAll().stream().forEach(task -> task.setStatus(TaskStatus.DONE));
 
-        manager.createSubTask(subtask1);
-        manager.createSubTask(subtask2);
-        manager.createSubTask(subtask3);
+        SubTask subtask10 = new SubTask(10, "СабТаска1", "СабТаска1_Тест", TaskStatus.DONE, epicId);
+        SubTask subtask11 = new SubTask(11, "СабТаска2", "СабТаска2_Тест", TaskStatus.DONE, epicId);
+        SubTask subtask12 = new SubTask(12, "СабТаска3", "СабТаска3_Тест", TaskStatus.DONE, epicId);
+
+        manager.createSubTask(subtask10);
+        manager.createSubTask(subtask11);
+        manager.createSubTask(subtask12);
 
         assertEquals("DONE", epicFromManager.getStatus().toString());
     }
